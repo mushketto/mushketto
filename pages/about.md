f5d388be0ada3c7d1dc2aef3b7f2b70dd12d2e4f
mushketto <dmitriy.berkman1@gmail.com>
2026-05-01 02:00:42 +0300

    docs

---
+++ pages/about.md
@@ -0,0 +1,4 @@
+# About the Author
+**Name:** Berkman Dmytro Valeriyovych
+**Specialty:** Software Engineering, ZNU
+**Project:** Hotel Booking System API (OAS/SDK Implementation)
---
+++ zudoku.config.tsx
@@ -0,0 +1,71 @@
+import type { ZudokuConfig } from "zudoku";
+
+const config: ZudokuConfig = {
+  site: {
+    logo: {
+      src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
+      alt: "Hotel Booking API",
+      width: "130px",
+    },
+    title: "Hotel API Docs",
+  },
+  navigation: [
+    {
+      type: "category",
+      label: "Documentation",
+      items: [
+        {
+          type: "category",
+          label: "Getting Started",
+          icon: "sparkles",
+          items: [
+            "/introduction",
+            "/installation",  
+            "/authorization", 
+          ],
+        },
+        {
+          type: "category",
+          label: "Resources",
+          icon: "hotel",
+          items: [
+            {
+              type: "link",
+              label: "API Reference",
+              to: "/api",
+              icon: "folder-cog",
+            },
+          ],
+        },
+        {
+          type: "category",
+          label: "Project Info",
+          icon: "user",
+          items: [
+            "/about", 
+          ],
+        },
+      ],
+    },
+    {
+      type: "link",
+      label: "About",
+      to: "/about",
+    },
+    {
+      type: "link",
+      to: "/api",
+      label: "API Reference",
+    },
+  ],
+  redirects: [{ from: "/", to: "/introduction" }],
+  apis: [
+    {
+      type: "file",
+      input: "./apis/openapi.yaml",
+      path: "/api",
+    },
+  ],
+};
+
+export default config;
