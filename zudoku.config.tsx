2d26a4a23618d6ccbcb99f6b3b6f238f0e997c7a
mushketto <dmitriy.berkman1@gmail.com>
2026-05-01 02:22:00 +0300

    edited

--- zudoku.config.tsx
+++ zudoku.config.tsx
@@ -1,8 +1,7 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
-  base: "/mushketto/",
-  
+
   site: {
     logo: {
       src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
